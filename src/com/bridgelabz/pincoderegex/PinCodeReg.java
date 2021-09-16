c7a196ead4d9665ffd4392f03cfe200d17e92199
govind6498 <govindsingh6498@gmail.com>
2021-09-16 14:04:30 +0530

    Added:Welcome to Pincode Validator

---
+++ src/com/bridgelabz/pincoderegex/PinCodeReg.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.pincoderegex;
+
+public class PinCodeReg {
+public static void main(String[] args) {
+	System.out.println("*********************************************");
+	System.out.println("        Welcome to Pin Code validator ");
+	System.out.println("*********************************************");
+}
+}
