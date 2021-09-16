488a2debbcc95c5b4fd7bb416bf6159fb5fa287c
govind6498 <govindsingh6498@gmail.com>
2021-09-16 14:24:42 +0530

    Added:Method to Valid 6 digit Pincode

--- src/com/bridgelabz/pincoderegex/PinCodeReg.java
+++ src/com/bridgelabz/pincoderegex/PinCodeReg.java
@@ -1,9 +1,25 @@
 package com.bridgelabz.pincoderegex;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PinCodeReg {
-public static void main(String[] args) {
-	System.out.println("*********************************************");
-	System.out.println("        Welcome to Pin Code validator ");
-	System.out.println("*********************************************");
-}
+	public static boolean checkValidPincode(String pinCode) {
+		String pinCodeRegex = "[0-9]{6}$";
+		Pattern patternObject = Pattern.compile(pinCodeRegex);
+		if (pinCode == null) {
+			return false;
+		}
+		Matcher matcherObject = patternObject.matcher(pinCode);
+		return matcherObject.matches();
+	}
+	public static void main(String[] args) {
+		String pincode="400088";
+		boolean isPinCode=checkValidPincode(pincode);
+		if(isPinCode) {
+			System.out.println(pincode+" is an Valid PinCode");
+			System.exit(0);
+		}
+		System.out.println(pincode+" is an Invalid PinCode");
+
+	}
 }
